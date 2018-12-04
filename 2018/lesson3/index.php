886926932d990b73ffd26070f47e93d627c0c299
ProgProgrammer <progprogrammer@yandex.ru>
2018-12-04 19:48:50 +0400

    My second site (lesson 3).

---
+++ 2018/lesson3/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>THE BLACK CASE</title>
+		<link rel="stylesheet" href="style.css">
+		<link rel="shortcut icon" href="img/logotwo.jpg" type="image/jpg">
+	</head>
+	<body>
+		<h1><a href="index.php">THE BLACK CASE</a></h1>
+		<h2><strong>+7(961)-6-949-949</strong></h2>
+		<h2><strong><a href="map.html">Контакты</a></strong></h2>
+		<nav class="top-menu">
+			<ul class="menu-main">
+				<li class="left-item"><a href="index.php" class="down">Телефоны</a></li>
+					<ul class="submenu">
+						<li class="left-item"><a href="apple.html">Apple</a></li>
+						<li class="left-item"><a href="samsungphone.html">Samsung</a></li>
+						<li class="left-item"><a href="meizuphone.html">Meizu</a></li>
+					</ul>
+				<li class="right-item"><a href="index.php" class="down">Чехлы</a></li>
+					<ul class="submenu">
+						<li class="right-item"><a href="applecase.html">Apple</a></li>
+						<li class="right-item"><a href="samsungcase.html">Samsung</a></li>
+						<li class="right-item"><a href="meizucase.html">Meizu</a></li>
+						<li class="right-item"><a href="cabinet.html">Кабинет</a></li>
+					</ul>
+			</ul>
+				<a class="navbar-logo" href="index.php"><img src="img/logo.jpg" alt="интернет-магазин"></a>
+		</nav>
+		<h3>Популярные смартфоны и чехлы</h3>
+		<figure class="pictureone">
+			<p><a href="flamingo.html"><img src="img/flamingo.jpg" width="150" height="200" alt="чехол на iphone фламинго"></a></p>
+			<figcaption><h4><a href="flamingo.html">Чехол Flamingo</a></h4></figcaption>
+		</figure>
+		<figure class="picturetwo">
+			<p><a href="pink.html"><img src="img/pink.jpg" width="150" height="200" alt="розовый чехол на iphone"></a></p>
+			<figcaption><h4><a href="pink.html">Чехол KRY</a></h4></figcaption>
+		</figure>
+		<figure class="picturethree">
+			<p><a href="kisscase.html"><img src="img/kisscase.jpg" width="150" height="200" alt="чехол kisscase на iphone"></a></p>
+			<figcaption><h4><a href="kisscase.html">Чехол Kisscase</a></h4></figcaption>
+		</figure>
+		<figure class="picturefour">
+			<p><a href="miezupro8.html"><img src="img/meizupro8.jpg" width="150" height="200" alt="смартфон miezu"></a></p>
+			<figcaption><h4><a href="miezupro8.html">Meizu Pro 8</a></h4></figcaption>
+		</figure>
+		<figure class="picturefive">
+			<p><a href="samsungs9.html"><img src="img/samsungs9.jpg" width="150" height="200" alt="смартфон samsung"></a></p>
+			<figcaption><h4><a href="samsungs9.html">Samsung Galaxy S9</a></h4></figcaption>
+		</figure>
+		<figure class="picturesix">
+			<p><a href="iphonexsmax.html"><img src="img/iphonexsmax.jpg" width="150" height="200" alt="iphone"></a></p>
+			<figcaption><h4><a href="smartphones/apple/iphonexsmax.html">Iphone XS Max</a></h4></figcaption>
+		</figure>
+		<div class="footer">
+			<a href="https://vk.com/the.black.case" target="_blank">THE BLACK CASE 2018 &copy SITE</a>
+		</div>
+	</body>
+</html>
