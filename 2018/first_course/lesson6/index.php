08840813ff70206c5d950254655a520c8ee63e17
ProgProgrammer <progprogrammer@yandex.ru>
2019-01-13 20:31:13 +0400

    My second site (lesson 6).

---
+++ 2018/first_course/lesson6/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width,initial-scale=1" />
+		<title>INTERIOR</title>
+		<link rel="stylesheet" href="style.css?v=1">
+		<link rel="shortcut icon" href="img/logotwo.png" type="image/png">
+	</head>
+	<body>
+        <div class="container">
+	        <div class="slider">
+	            <div class="header clearfix">
+	                <a class="logo" href="index.php"><img src="img/logo.png" alt="logotype"></a>
+                    <ul class="menu">
+                        <li><a class="text-style-one" href="index.php">HOME</a></li>
+                        <li><a href="product.html">PRODUCT</a></li>
+                        <li><a href="history.html">HISTORY</a></li>
+                        <li><a href="showroom.html">SHOWROOM</a></li>
+                        <li><a href="contact.html">CONTACT</a></li>
+                        <li><a class="search" href="search.html"><img src="img/search.png" alt="search"></a></li>
+                    </ul>
+	            </div>
+	            <div class="text-box">
+	                <h3 class="tag">TRENDING</h3>
+	                <h2 class="name">Fishnet Chair</h2>
+	                <p class="details">
+                        Seat and back with upholstery made of cold cure foam. Steel frame, available in matt powder-coated black
+                        or highly polished chrome.
+                    </p>
+	                <a class="back" href="index.php">Order Us</a>
+	            </div>
+	            <div class="navi">
+	                <a href="#"><img src="img/navi.png" alt="navi"></a>
+	            </div>
+	            <div id="layer" class="navi2">
+	                <a href="#"><img src="img/active2.png" alt="active"></a>
+	            </div>
+	        </div>
+	        <div class="featured">
+	            <div class="header clearfix">
+	                <ul class="menutwo">
+	                    <li><a class="lione" href="index.php">Featured</a></li>
+	                    <li><a class="litwo" href="index.php">New</a></li>
+	                </ul>
+	                <div class="explore-all">
+	                    <a href="index.php">explore all</a>
+	                </div>
+	            </div>
+	            <div class="boxs">
+	                <div class="product1">
+	                    <img src="img/product1.jpg" alt="product1">
+                    </div>
+	                <div class="product1back"></div>
+	                <div id="layer" class="text-box2">
+	                    <div class="back2">
+	                        <a href="index.php"><img src="img/arrow2.png" alt="arrow"></a>
+	                    </div>
+	                    <h2 id="layer2" class="name2">Fishnet Chair</h2>
+	                    <p id="layer2" class="details2">
+	                        Seat and back with upholstery made of cold cure
+	                    </p>
+	                </div>
+	                <div class="product2">
+	                    <a href="index.php"><img src="img/product2.jpg" alt="product"></a>
+	                </div>
+	                <div class="product3">
+	                    <a href="index.php"><img src="img/product3.jpg" alt="product"></a>
+	                </div>
+	                <div class="product4">
+	                    <a href="index.php"><img src="img/product4.jpg" alt="product"></a>
+	                </div>
+	            </div>
+	        </div>
+            <div class="promo">
+                <div class="promo-img1">
+                    <div class="text-box3">
+                        <h3 class="tag3">exclusive</h3>
+                        <h2 class="name3">Fishnet Chair</h2>
+                        <p class="details3">
+                            Seat and back with upholstery made of cold cure foam. Steel frame, available in matt
+                        </p>
+                        <a href="index.php" class="back3">Order Us</a>
+                    </div>
+                </div>
+                <div class="promo-img2">
+                    <div class="text-box3">
+                        <h3 class="tag3">exclusive</h3>
+                        <h2 class="name3">Fishnet Chair</h2>
+                        <p class="details3">
+                            Seat and back with upholstery made of cold cure foam. Steel frame, available in matt
+                        </p>
+                        <a href="index.php" class="back3">Order Us</a>
+                    </div>
+                </div>
+            </div>
+	    </div>
+	</body>
