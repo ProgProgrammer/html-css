57349b22ca035f28d6505ccaf47806ff3327515b
ProgProgrammer <progprogrammer@yandex.ru>
2019-01-22 22:25:51 +0400

    My second website (lesson 6).

---
+++ 2018/first_course/lesson7/index.php
@@ -0,0 +1,271 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width">
+		<title>INTERIOR</title>
+		<link rel="stylesheet" href="style.css?v=1">
+		<link rel="shortcut icon" href="img/logotwo.png" type="image/png">
+		<link href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" rel="stylesheet">
+	</head>
+	<body>
+        <div class="container">
+	        <div class="content">
+	            <div class="slider">
+	                <div class="header clearfix">
+	                    <a class="logo" href="index.php"><img src="img/logo.png" alt="logotype"></a>
+                        <ul class="menu">
+                            <li><a class="text-style-one" href="index.php">HOME</a></li>
+                            <li><a href="index.php">PRODUCT</a></li>
+                            <li><a href="index.php">HISTORY</a></li>
+                            <li><a href="index.php">SHOWROOM</a></li>
+                            <li><a href="index.php">CONTACT</a></li>
+                            <a class="search" href="index.php"><img src="img/search.png" alt="search"></a>
+                        </ul>
+	                </div>
+	                <div class="text-box">
+	                    <h3 class="tag">TRENDING</h3>
+	                    <h2 class="name">Fishnet Chair</h2>
+	                    <p class="details">
+                            Seat and back with upholstery made of&nbsp;cold cure foam. Steel frame, available in&nbsp;matt powder-coated black
+                        </p>
+	                    <a class="back" href="index.php">Order Us</a>
+	                </div>
+	                <div class="navi">
+	                    <a class="circle" href="index.php"></a>
+	                    <a class="circle_active" href="index.php"></a>
+	                    <a class="circle" href="index.php"></a>
+	                </div>
+                </div>
+	            <div class="featured">
+	                <div class="header clearfix">
+	                    <ul class="menutwo">
+	                        <li><a class="lione" href="index.php">Featured</a></li>
+	                        <li><a class="litwo" href="index.php">New</a></li>
+	                    </ul>
+	                    <div class="explore-all">
+	                        <a href="index.php">explore all</a>
+	                    </div>
+	                </div>
+	                <div class="boxs">
+	                    <div class="product1">
+	                        <div id="layer" class="text-box2">
+                                <div class="background">
+	                                <h2 class="name2">Fishnet Chair</h2>
+	                                <p id="layer2" class="details2">
+	                                    Seat and back with upholstery made of&nbsp;cold cure
+	                                </p>
+	                            </div>
+	                            <a class="back2" href="index.php"><img src="img/arrow2.png" alt="arrow"></a>
+	                        </div>
+	                    </div>
+	                    <div class="product2">
+	                        <div id="layer" class="text-box21">
+                                <div class="background">
+	                                <h2 id="layer2" class="name2">Fishnet Chair</h2>
+	                                <p id="layer2" class="details2">
+	                                    Seat and back with upholstery made of&nbsp;cold cure
+	                                </p>
+	                            </div>
+	                            <a class="back2" href="index.php"><img src="img/arrow2.png" alt="arrow"></a>
+	                        </div>
+	                    </div>
+	                    <div class="product3">
+	                        <div id="layer" class="text-box22">
+                                <div class="background">
+	                                <h2 id="layer2" class="name2">Fishnet Chair</h2>
+	                                <p id="layer2" class="details2">
+	                                    Seat and back with upholstery made of&nbsp;cold cure
+	                                </p>
+	                            </div>
+	                            <a class="back2" href="index.php"><img src="img/arrow2.png" alt="arrow"></a>
+	                        </div>
+	                    </div>
+	                    <div class="product4">
+	                        <div id="layer" class="text-box23">
+                                <div class="background">
+	                                <h2 id="layer2" class="name2">Fishnet Chair</h2>
+	                                <p id="layer2" class="details2">
+	                                    Seat and back with upholstery made of&nbsp;cold cure
+	                                </p>
+	                            </div>
+	                            <a class="back2" href="index.php"><img src="img/arrow2.png" alt="arrow"></a>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+                <div class="promo">
+                    <div class="promo-img1">
+                        <div class="text-box3">
+                            <h3 class="tag3">exclusive</h3>
+                            <h2 class="name3">Fishnet Chair</h2>
+                            <p class="details3">
+                                Seat and back with upholstery made of&nbsp;cold cure foam. Steel frame, available in&nbsp;matt
+                            </p>
+                            <a href="index.php" class="back3">Order Us</a>
+                        </div>
+                    </div>
+                    <div class="promo-img2">
+                        <div class="text-box3">
+                            <h3 class="tag3">exclusive</h3>
+                            <h2 class="name3">Fishnet Chair</h2>
+                            <p class="details3">
+                                Seat and back with upholstery made of&nbsp;cold cure foam. Steel frame, available in&nbsp;matt
+                            </p>
+                            <a href="index.php" class="back3">Order Us</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="products">
+                    <div class="header clearfix">
+                        <p class="trending">Trending Products</p>
+                        <div class="explorer-all2">
+                            <a href="index.php">explore all</a>
+                        </div>
+                    </div>
+                    <div class="boxs2">
+                        <div class="product5">
+                            <a href="index.php"><img src="img/product5.jpg" alt="product 5"></a>
+                        </div>
+                        <div class="product6">
+                            <a href="index.php"><img src="img/product6.jpg" alt="product 6"></a>
+                        </div>
+                        <div class="product7">
+                            <a href="index.php"><img src="img/product7.jpg" alt="product 7"></a>
+                        </div>
+                        <div class="product8">
+                            <a href="index.php"><img src="img/product8.jpg" alt="product 8"></a>
+                        </div>
+                        <div class="product9">
+                            <a href="index.php"><img src="img/product9.jpg" alt="product 9"></a>
+                        </div>
+                        <div class="product10">
+                            <a href="index.php"><img src="img/product10.jpg" alt="product 10"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="slider2">
+                    <div class="text-box4 clearfix">
+                        <h3 class="tag4">Hot deal</h3>
+                        <h2 class="name4">Fishnet Chair</h2>
+                        <p class="details4">
+                            Seat and back with upholstery made of&nbsp;cold cure foam. Steel frame, available in&nbsp;matt powder-coated black
+                        </p>
+                        <a class="back4" href="index.php">Order Us</a>
+                    </div>
+                    <div class="navi2">
+	                    <a class="circle" href="index.php"></a>
+	                    <a class="circle_active" href="index.php"></a>
+	                    <a class="circle" href="index.php"></a>
+	                </div>
+                </div>
+                <div class="promo2">
+                    <div class="promo-img3">
+                        <div class="text-box5">
+                            <h3 class="tag5">exclusive</h3>
+                            <h2 class="name5">Fishnet Chair</h2>
+                            <p class="details5">
+                                Seat and back wupholstery made of&nbsp;cold cure foam. Steel frame, availain matt
+                            </p>
+                            <a class="back5" href="index.php">order us</a>
+                        </div>
+                    </div>
+                    <div class="promo-img4">
+                        <div class="product11">
+                            <a href="index.php"><img src="img/product-2-sm.jpg" alt="product"></a>
+                        </div>
+                        <div class="product12">
+                            <a href="index.php"><img src="img/product-3-sm.jpg" alt="product"></a>
+                        </div>
+                        <div class="product13">
+                            <a href="index.php"><img src="img/product-4-sm.jpg" alt="product"></a>
+                        </div>
+                        <div class="product14">
+                            <a href="index.php"><img src="img/product-5-sm.jpg" alt="product"></a>
+                        </div>
+                    </div>
+                </div>
+                <div class="back-img">
+                    <h3 class="tag6">mobile app</h3>
+                    <h2 class="name6">Find. Follow. Favorite.</h2>
+                    <p class="details6">
+                        Save your favorites and share your style.
+                    </p>
+                    <div class="links">
+                        <a href="https://play.google.com/store/apps?hl=ru" target="_blank"><img src="img/app-store.jpg" alt="app-store"></a>
+                        <a href="https://www.apple.com/ru/ios/app-store/" target="_blank"><img src="img/google-play.jpg" alt="google-store"></a>
+                    </div>
+                </div>
+	        </div>
+            <div class="footer-back">
+               <div class="contact-information">
+                <div class="contact_us">
+                    <h3 class="contact">
+                        Contact Us
+                    </h3>
+                    <p class="adress_up">
+                        132A Bridge Road Richmond VIC 
+                        Australia.
+                    </p>
+                    <p class="adress_down">
+                        Talk to&nbsp;us on&nbsp;1300&nbsp;132
+                        <a href="mailto:info@interior.com ?subject=User's message &Body=Good afternoon. &cc=zubr.free@yandex.ru &bcc=vladimirandreev260@gmail.com">info@interior.com</a>
+                    </p>
+                </div>
+                <div class="information">
+                    <h3 class="contact">Useful Information</h3>
+                    <div class="information2">
+                        <ul class="info1">
+                            <li><a href="index.php">Sales terms</a></li>
+                            <li><a href="index.php">Customer care</a></li>
+                            <li><a href="index.php">Delivery</a></li>
+                        </ul>
+                        <ul class="info2">
+                            <li><a href="index.php">Architect accounts</a></li>
+                            <li><a href="index.php">Careers</a></li>
+                            <li><a href="index.php">Exchanges & returns</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="touch">
+                    <h3 class="contact1">Let’s Stay in Touch!</h3>
+                    <p class="write">
+                       Suscribe to&nbsp;know about our latest news, products and special offers just for suscribers.
+                    </p>
+                    <form action="index.php">
+                        <div class="email">
+                            <input type="email" placeholder="your email address" name="email" required>
+                            <a href="index.php"><i class="fab fa-telegram-plane"></i></a>
+                        </div>
+                    </form>
+                </div>
+               </div>
+               <div class="links2">
+                    <div class="copyright">
+                        <p>
+                             &copy;&nbsp;Copyright&nbsp;&mdash; INTERIOR 2016. All Rights Reserved.
+                        </p>
+                    </div>
+                    <div class="social-links">
+                        <a target="_blank" href="https://ru-ru.facebook.com/">
+                            <i class="fab fa-facebook-square"></i>
+                        </a>
+                        <a target="_blank" href="https://twitter.com/?lang=ru">
+                            <i class="fab fa-twitter-square"></i>
+                        </a>
+                        <a target="blank" href="https://plus.google.com/discover">
+                            <i class="fab fa-google-plus-square"></i>
+                        </a>
+                        <a target="_blank" href="https://www.pinterest.ru/">
+                            <i class="fab fa-pinterest-square"></i>
+                        </a>
+                    </div>
+                    <div class="privacy">
+                        <p>
+                            <a href="index.php">Terms &amp;&nbsp;Conditions&nbsp;</a>/ <a href="index.php">Privacy policy &amp;&nbsp;Cookies</a>
+                        </p>
+                    </div>
+               </div>
+            </div>
+	    </div>
+	</body>
