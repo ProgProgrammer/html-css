1d5ed175f1cce99d0fac8a8f8e08827ea5cc1619
ProgProgrammer <progr.programmer@yandex.ru>
2019-02-14 11:43:46 +0400

    Responsive website (Product).

--- 2018/second_course/lesson_2/index.php
+++ 2018/second_course/lesson_2/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PRODUCT</title>
     <link rel="stylesheet" href="style.css?v=1">
     <link rel="shortcut icon" href="img/logotwo.png" type="image/png">
