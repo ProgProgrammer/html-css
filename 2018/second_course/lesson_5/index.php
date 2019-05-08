85a8806bfede9d1f707ab020238aeb4660a34abd
ProgProgrammer <progr.programmer@yandex.ru>
2019-05-08 16:44:16 +0400

    Fourth animation website.

---
+++ 2018/second_course/lesson_5/index.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Paradigm Shift</title>
+    <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+    <link rel="stylesheet" href="style.css?v=1">
+    <link rel="shortcut icon" href="img/logotwo.jpg" type="image/jpg">
+    <link rel="stylesheet" href="reset.css">
+    <link href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <noscript>Вы видите это сообщение, потому что Ваш браузер не поддерживает JavaScript.</noscript>
+</head>
+<body>
+<div class="container">
+    <div class="row content content_100">
+        <header id="animateOne" class="col-md-6 header header_animation header_margin-top">
+            <div class="header_blocks">
+                <div id="animateThree" class="header_block1 header_width header_animation2">
+                    <h1>Paradigm Shift</h1>
+                    <p>
+                        A FREE RESPONSIVE SITE TEMPLATE
+                        <br>
+                        DESIGNED BY GitHub / HTML5 UP
+                    </p>
+                    <ul>
+                        <li></li>
+                    </ul>
+                    <div id="arrow" class="header_block1_click">
+                        <a href="#bottom" class="header_block1_click_arrow"></a>
+                    </div>
+                </div>
+                <div class="header_block2">
+                    <div class="header_block2_circle"></div>
+                    <div class="header_block2_line"></div>
+                </div>
+            </div>
+        </header>
+        <main id="animateTwo" class="col-md-6 main header_animation3">
+            <img class="main_img main_img_margin" src="img/pic01.jpg" alt="image">
+        </main>
+    </div>
+    <div id="bottom" class="row content">
+        <header class="col-md-6 header header_not_padding">
+            <div class="header_blocks">
+                <div class="header_block1">
+                    <h2>
+                        MAGNA SED NULLAM NISL ADIPISCING
+                    </h2>
+                </div>
+                <div class="header_block2 margin_px">
+                    <div class="header_block2_circle header_margin-top1"></div>
+                    <div class="header_block2_line header_margin-top2"></div>
+                </div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <p class="main_p"><strong>Lorem ipsum dolor</strong> sit amet consectetur adipiscing elit. Duis dapibus rutrum facilisis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Etiam tristique libero eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.</p>
+            <img src="img/pic02.jpg" alt="" class="main_img">
+        </main>
+    </div>
+    <div class="row content">
+        <header class="col-md-6 header header_not_padding">
+            <div class="header_blocks">
+                <div class="header_block1">
+                    <h2>
+                        FEUGIAT CONSEQUAT TEMPUS ULTRICES
+                    </h2>
+                </div>
+                <div class="header_block2 margin_px">
+                    <div class="header_block2_circle header_margin-top3"></div>
+                    <div class="header_block2_line header_margin-top5"></div>
+                </div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <p class="main_p transform_none"><strong>Etiam tristique libero</strong> eu nibh porttitor amet fermentum. Nullam venenatis erat id vehicula ultrices sed ultricies condimentum.</p>
+            <ul class="row main_ul">
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-laptop"></i>
+                    </div>
+                    <p class="main_li_p">Consequat tempus</p>
+                </li>
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-bolt"></i>
+                    </div>
+                    <p class="main_li_p">Etiam adipiscing</p>
+                </li>
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-signal"></i>
+                    </div>
+                    <p class="main_li_p">Libero nullam</p>
+                </li>
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-cog"></i>
+                    </div>
+                    <p class="main_li_p">Blandit condimentum</p>
+                </li>
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-map-marker"></i>
+                    </div>
+                    <p class="main_li_p">Lorem ipsum dolor</p>
+                </li>
+                <li class="col-md-6 main_ul_li main_ul_li_margin">
+                    <div class="li_geometry">
+                        <i class="fas fa-code"></i>
+                    </div>
+                    <p class="main_li_p">Nibh amet venenati</p>
+                </li>
+            </ul>
+            <p class="main_p transform_none main_p_margin_bottom">
+                Vehicula ultrices sed ultricies condimentum. Magna sed etiam consequat, et lorem adipiscing sed nulla. Volutpat nisl et tempus et dolor libero, feugiat magna tempus, sed et lorem adipiscing.
+            </p>
+        </main>
+    </div>
+    <div class="row content width_50">
+        <header class="col-md-6 header header_not_padding">
+            <div class="header_blocks">
+                <div class="header_block1">
+                    <h2>
+                        ULTRICES ERAT MAGNA SED CONDIMENTUM
+                    </h2>
+                </div>
+                <div class="header_block2 margin_px header_block2_linear2">
+                    <div class="header_block2_circle header_margin-top3"></div>
+                    <div class="header_block2_line header_margin-top5"></div>
+                </div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <p class="main_p transform_none"><strong>Integer mollis egestas</strong> nam maximus erat id euismod egestas. Pellentesque sapien ac quam. Lorem ipsum dolor sit nullam.</p>
+        </main>
+    </div>
+    <div class="row content width_50">
+        <header class="col-md-6 header header_not_padding not_background main_main_padding">
+            <div class="header_blocks header_blocks2">
+                <div class="header_block1">
+                    <h3>
+                        ERAT ALIQUAM
+                    </h3>
+                    <p class="header_block1_p">
+                        Vehicula ultrices dolor amet ultricies et condimentum. Magna sed etiam consequat, et lorem adipiscing sed dolor sit amet, consectetur amet do eiusmod tempor incididunt ipsum suspendisse ultrices gravida.
+                    </p>
+                </div>
+                <div class="header_block2 margin_px header_block2_linear"></div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main main_main_padding">
+            <a class="main_a" href="#">
+                <img src="img/01.jpg" alt="">
+            </a>
+            <div class="main_space">
+                <a class="main_a margin_top2 main_img_width" href="#">
+                    <img src="img/02.jpg" alt="">
+                </a>
+                <a class="main_a margin_top2 main_img_width" href="#">
+                    <img src="img/03.jpg" alt="">
+                </a>
+            </div>
+            <a class="main_a" href="#">
+                <img class="margin_top2" src="img/04.jpg" alt="">
+            </a>
+        </main>
+    </div>
+    <div class="row content width_50">
+        <header class="col-md-6 header header_not_padding not_background3">
+            <div class="header_blocks header_blocks2">
+                <div class="header_block1">
+                    <h3>
+                        NISL CONSEQUAT
+                    </h3>
+                    <p class="header_block1_p">
+                        Aenean ornare velit lacus, ac varius enim ullamcorper eu. Proin aliquam sed facilisis ante interdum congue. Integer mollis, nisl amet convallis, porttitor magna ullamcorper, amet mauris. Ut magna finibus nisi nec lacinia ipsum maximus.
+                    </p>
+                </div>
+                <div class="header_block2 margin_px header_block2_linear"></div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <div>
+                <a class="main_a" href="#">
+                    <img src="img/05.jpg" alt="">
+                </a>
+                <div class="main_space">
+                    <a class="main_a margin_top2 main_img_width" href="#">
+                        <img src="img/06.jpg" alt="">
+                    </a>
+                    <a class="main_a margin_top2 main_img_width" href="#">
+                        <img src="img/07.jpg" alt="">
+                    </a>
+                </div>
+            </div>
+        </main>
+    </div>
+    <div class="row content width_50">
+        <header class="col-md-6 header header_not_padding not_background4">
+            <div class="header_blocks header_blocks2">
+                <div class="header_block1">
+                    <h3>
+                        LOREM GRAVIDA
+                    </h3>
+                    <p class="header_block1_p">
+                        Proin aliquam facilisis ante interdum. Sed nulla amet lorem feugiat tempus aenean ornare velit lacus, ac varius sed enim lorem ullamcorper dolore. ac varius enim lorem ullamcorper dolore. Proin aliquam facilisis.
+                    </p>
+                </div>
+                <div class="header_block2 margin_px"></div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main main_main_padding">
+            <div class="main_space">
+                <a class="main_a margin_top3 main_img_width" href="#">
+                    <img src="img/08.jpg" alt="">
+                </a>
+                <a class="main_a margin_top3 main_img_width" href="#">
+                    <img src="img/09.jpg" alt="">
+                </a>
+            </div>
+            <a class="main_a" href="#">
+                <img class="margin_top2" src="img/10.jpg" alt="">
+            </a>
+        </main>
+    </div>
+    <div class="row content width_50">
+        <header class="col-md-6 header header_not_padding">
+            <div class="header_blocks">
+                <div class="header_block1">
+                    <h2>
+                        DUIS SED ADPISCING VEROEROS AMET
+                    </h2>
+                </div>
+                <div class="header_block2 margin_px header_block2_linear2">
+                    <div class="header_block2_circle header_margin-top3"></div>
+                    <div class="header_block2_line header_margin-top5"></div>
+                </div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <p class="main_p main_p_margin_bottom transform_none"><strong>Proin tempus feugiat</strong> sed varius enim lorem ullamcorper dolore aliquam aenean ornare velit lacus, ac varius enim lorem ullamcorper dolore.</p>
+            <div class="main_buttons">
+                <a href="#" class="main_button primary">get started</a>
+                <a href="#" class="main_button border_button">learn more</a>
+            </div>
+        </main>
+    </div>
+    <div class="row content content_footer width_50">
+        <header class="col-md-6 header header_not_padding">
+            <div class="header_blocks">
+                <div class="header_block1 header_block1_height">
+                    <h2>
+                        GET IN TOUCH
+                    </h2>
+                </div>
+                <div class="header_block2 margin_px header_block2_height">
+                    <div class="header_block2_circle header_margin-top3"></div>
+                    <div class="header_block2_line header_margin-top5"></div>
+                    <div class="header_block2_circle2"></div>
+                </div>
+            </div>
+        </header>
+        <main class="col-md-6 main_main">
+            <p class="main_p main_p_margin_bottom transform_none"><strong>Auctor commodo</strong> interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque venenatis dolor imperdiet dolor mattis sagittis.</p>
+            <form action="#" class="row form">
+                <div class="form_input col-md-5">
+                    <input type="text" placeholder="Имя">
+                </div>
+                <div class="form_input2 col-md-5">
+                    <input type="email" placeholder="Email">
+                </div>
+                <div class="col-md-12 form_textarea">
+                    <textarea name="message" placeholder="Письмо" id="message" rows="7"></textarea>
+                </div>
+                <div class="main_main_button">
+                    <button type="submit">
+                        send message
+                    </button>
+                </div>
+            </form>
+
+        </main>
+        <footer class="col-md-6 footer">
+            <ul class="footer_ul">
+                <li class="footer_ul_li">
+                    <h3 class="footer_ul_li_h3">
+                        email
+                    </h3>
+                    <a href="mailto:vladimirandreev260@gmail.com" class="footer_ul_li_a">
+                        vladimirandreev260@gmail.com
+                    </a>
+                </li>
+                <li class="footer_ul_li">
+                    <h3 class="footer_ul_li_h3">
+                        phone
+                    </h3>
+                    <a href="tel:+79178311783" class="footer_ul_li_a">
+                        (917)83-117-83
+                    </a>
+                </li>
+                <li class="footer_ul_li">
+                    <h3 class="footer_ul_li_h3">
+                        address
+                    </h3>
+                    <a class="footer_ul_li_a border_none">
+                        1234 Somewhere Road, Nashville, TN 00000
+                    </a>
+                </li>
+                <li class="footer_ul_li margin_none">
+                    <h3 class="footer_ul_li_h3">
+                        ELSEWHERE
+                    </h3>
+                    <ul class="footer_ul_li_ul">
+                        <li><a href="#">
+                            <i class="fab fa-twitter"></i>
+                        </a></li>
+                        <li><a href="#">
+                            <i class="fab fa-facebook"></i>
+                        </a></li>
+                        <li><a href="#">
+                            <i class="fab fa-instagram"></i>
+                        </a></li>
+                        <li><a href="#">
+                            <i class="fab fa-linkedin-in"></i>
+                        </a></li>
+                        <li><a href="#">
+                            <i class="fab fa-github"></i>
+                        </a></li>
+                        <li><a href="#">
+                            <i class="fab fa-codepen"></i>
+                        </a></li>
+                    </ul>
+                </li>
+                <li class="footer_ul_li mobile_none"></li>
+            </ul>
+            <script>
+                $(document).ready(function(){
+                    $("#arrow").on("click","a", function (event) {
+                        event.preventDefault();
+                        var id  = $(this).attr('href'),
+                            top = $(id).offset().top;
+                        $('body,html').animate({scrollTop: top}, 1500);
+                    });
+                });
+            </script>
+            <script src="js/animation.js"></script>
+        </footer>
+    </div>
+    <div class="row copyright">
+        <div class="col-md-6 copyright_text">
+            &copy Untitled. All rights reserved <a href="https://github.com/ProgProgrammer">GitHub</a>. Design:
+            <a href="https://html5up.net" target="_blank">HTML5 UP</a>
+            . Demo Images:
+            <a href="https://unsplash.com" target="_blank">Unsplash</a>
+            .
+        </div>
+    </div>
+</div>
+</body>
+</html>
---
+++ 2018/second_course/lesson_5/js/animation.js
@@ -0,0 +1,28 @@
+var script = document.createElement('script');
+script.src = "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.3.0/lodash.js"
+document.body.appendChild(script);
+
+script.onload = function() {
+    var mql = window.matchMedia('all and (min-width: 1281px)');
+    var mql2 = window.matchMedia('all and (min-width: 1025px) and (max-width: 1280px)');
+    var mql3 = window.matchMedia('all and (max-width: 1024px)');
+    if (mql.matches) {
+        const x = document.getElementById("animateOne");
+        x.classList.add("animation");
+
+        const y = document.getElementById("animateTwo");
+        y.classList.add("animationtwo");
+
+    } else if (mql2.matches) {
+        const x = document.getElementById("animateThree");
+        x.classList.add("animationthree");
+
+        const y = document.getElementById("animateTwo");
+        y.classList.add("animationtwo");
+
+    } else if (mql3.matches) {
+        const x = document.getElementById("animateThree");
+        x.classList.add("animationfour");
+
+    }
+}
