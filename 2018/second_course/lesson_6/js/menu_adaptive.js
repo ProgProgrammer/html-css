a2b08b9b44e827fe0e2cf40ba6282471f36f81c2
Vladimir Andreev <progr.programmer@yandex.ru>
2019-06-24 22:45:11 +0400

    Animation website 2.

--- 2018/second_course/lesson_6/js/menu_adaptive.js
+++ 2018/second_course/lesson_6/js/menu_adaptive.js
@@ -1,12 +1,12 @@
+var block = document.getElementById("menu_block");
+var adaptive = document.getElementById("menu_absolute");
+
 function menuAdaptiveFunction() {
-    var block = document.getElementById("menu_block");
 
     block.classList.remove("menu_block_animation_off");
 
     block.classList.add("menu_block_animation");
 
-
-    var adaptive = document.getElementById("menu_absolute");
 
     adaptive.classList.remove("menu_absolute_animation_off");
 
@@ -14,14 +14,12 @@ function menuAdaptiveFunction() {
 }
 
 function menuCrossFunction() {
-    var block = document.getElementById("menu_block");
 
     block.classList.remove("menu_block_animation");
 
     block.classList.add("menu_block_animation_off");
 
 
-    var adaptive = document.getElementById("menu_absolute");
 
     adaptive.classList.remove("menu_absolute_animation");
 
